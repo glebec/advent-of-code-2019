c13bda7c05e651c7ed18a8ceeaa2efc9af1f0583
Gabriel Lebec <glebec@gmail.com>
2019-12-01 01:23:32 -0500

    day 01: set up ghc-options

---
+++ src/Day01/Solution.hs
@@ -0,0 +1,6 @@
+module Day01.Solution (result) where
+
+import Day01.Input (raw)
+
+result :: String
+result = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,8 @@
 module Main where
 
+import qualified Day01.Solution as D01
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+    putStrLn "Day 01:"
+    print D01.result
