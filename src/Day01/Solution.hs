938688f38d1d5f55b23b1548a03c53fd5da856bf
Gabriel Lebec <glebec@gmail.com>
2019-12-01 01:49:49 -0500

    day 01: part 1

--- src/Day01/Solution.hs
+++ src/Day01/Solution.hs
@@ -2,5 +2,11 @@ module Day01.Solution (result) where
 
 import Day01.Input (raw)
 
-result :: String
-result = undefined
+masses :: [Int]
+masses = read <$> lines raw
+
+getFuel :: Int -> Int
+getFuel f = f `quot` 3 - 2
+
+result :: Int
+result = sum $ getFuel <$> masses
