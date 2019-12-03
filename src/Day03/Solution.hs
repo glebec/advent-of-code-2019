7fd1900f988ea3f838f3149702593f12f08d36e0
Gabriel Lebec <glebec@gmail.com>
2019-12-03 01:53:27 -0500

    day 03: part 1

--- src/Day03/Solution.hs
+++ src/Day03/Solution.hs
@@ -1,33 +1,54 @@
-{-# LANGUAGE LambdaCase #-}
-
 module Day03.Solution (result1, result2) where
 
-import Prelude hiding (Left, Right)
-
 import Data.List.Split (splitOn)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 import Day03.Input (raw)
 
 -- parse
 
-data Direction = Up | Down | Left | Right deriving Eq
+data Direction = U | D | L | R deriving (Eq, Read)
 data Move = Move Direction Int
 
 parseMove :: String -> Move
-parseMove (d:x) =
-    let distance = read x
-    in  case d of
-        'U' -> Move Up distance
-        'D' -> Move Down distance
-        'L' -> Move Left distance
-        'R' -> Move Right distance
+parseMove (d:x) = Move (read [d]) (read x)
 parseMove move = error $ "Cannot parse move: " ++ move
 
--- programList :: [Instruction]
--- programList = read <$> splitOn "," raw
+parseMoves :: String -> [Move]
+parseMoves = fmap parseMove . splitOn ","
+
+moves :: ([Move], [Move])
+moves = case parseMoves <$> lines raw of
+    [a, b] -> (a, b)
+    _ -> error "Cannot parse input lines into moves."
 
 -- solve
 
-result1, result2 :: Int
-result1 = undefined
+type Coords = (Int, Int)
+
+trace :: [Move] -> [Coords]
+trace = go (0, 0) where
+    go xy [] = [xy]
+    go xy@(x, y) (move:ms') = case move of
+        Move _ d | d == 0 -> go xy ms'
+        Move U d -> xy : go (x, y + 1) (Move U (d - 1) : ms')
+        Move D d -> xy : go (x, y - 1) (Move D (d - 1) : ms')
+        Move L d -> xy : go (x - 1, y) (Move L (d - 1) : ms')
+        Move R d -> xy : go (x + 1, y) (Move R (d - 1) : ms')
+
+wire1, wire2 :: Set Coords
+wire1 = Set.fromList . tail . trace $ fst moves
+wire2 = Set.fromList . tail . trace $ snd moves
+
+crossings :: Set Coords
+crossings = Set.intersection wire1 wire2
+
+manhattanDistances :: Set Int
+manhattanDistances = Set.map (\(x, y) -> abs x + abs y) crossings
+
+result1 :: Int
+result1 = head . Set.toAscList $ manhattanDistances
+
+result2 :: Int
 result2 = undefined
--- src/Main.hs
+++ src/Main.hs
@@ -2,8 +2,10 @@ module Main where
 
 import qualified Day01.Solution as D01
 import qualified Day02.Solution as D02
+import qualified Day03.Solution as D03
 
 main :: IO ()
 main = do
     putStrLn $ "Day 01: " ++ show D01.result1 ++ " " ++ show D01.result2
     putStrLn $ "Day 02: " ++ show D02.result1 ++ " " ++ show D02.result2
+    putStrLn $ "Day 03: " ++ show D03.result1
