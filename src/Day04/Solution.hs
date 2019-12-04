b413e264503d8386b421a714267b879b10e80592
Gabriel Lebec <glebec@gmail.com>
2019-12-04 00:38:50 -0500

    day 04: rename <&&>

--- src/Day04/Solution.hs
+++ src/Day04/Solution.hs
@@ -13,12 +13,12 @@ monotonicUp :: Int -> Bool
 monotonicUp = check . show where
     check s = and $ zipWith (<=) s (tail s)
 
-infixl 3 &&&
-(&&&) :: (a -> Bool) -> (a -> Bool) -> (a -> Bool)
-(&&&) p q = (&&) <$> p <*> q
+infixl 3 <&&>
+(<&&>) :: Applicative a => a Bool -> a Bool -> a Bool
+(<&&>) p q = (&&) <$> p <*> q
 
 result1 :: Int
-result1 = length . filter (isSixDigits &&& twoAdjacentSame &&& monotonicUp) $ [low..high]
+result1 = length . filter (isSixDigits <&&> twoAdjacentSame <&&> monotonicUp) $ [low..high]
 
 result2 :: Int
 result2 = undefined
