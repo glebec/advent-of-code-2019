a694303bffb3e05625a7af31f49731e087d86bfb
Gabriel Lebec <glebec@gmail.com>
2019-12-08 21:11:08 -0500

    day 06: cleanup

--- src/Day06/Solution.hs
+++ src/Day06/Solution.hs
@@ -14,7 +14,6 @@ import qualified Data.Set as Set
 import Day06.Input (raw)
 
 -- parse
-
 newtype Satellite =
     Satellite String
     deriving (Eq, Ord, Show)
@@ -39,7 +38,6 @@ orbitList :: [Orbit]
 orbitList = parseOrbit <$> lines raw
 
 -- solve
-
 getTree :: [Orbit] -> Map Satellite Center
 getTree = Map.fromList . fmap unOrbit
 
@@ -59,31 +57,21 @@ result1 :: Int
 result1 = sum . fmap length . getRootPaths . getTree $ orbitList
 
 -- part 2
-
 findCommonOrbit :: Map Satellite [String] -> String
 findCommonOrbit m =
     let santaOrbits = Set.fromList (Map.findWithDefault [] (Satellite "SAN") m)
-    in  fromMaybe "" $
+     in fromMaybe "" $
         find
             (`Set.member` santaOrbits)
             (Map.findWithDefault [] (Satellite "YOU") m)
 
+getJumpsBtwYouSan :: Map Satellite [String] -> Maybe Int
+getJumpsBtwYouSan m = do
+    let common = findCommonOrbit m
+    santaDist <- elemIndex common (Map.findWithDefault [] (Satellite "SAN") m)
+    youDist <- elemIndex common (Map.findWithDefault [] (Satellite "YOU") m)
+    pure $ youDist + santaDist
+
 result2 :: Int
 result2 =
-    let paths :: Map Satellite [String]
-        paths = getRootPaths . getTree $ orbitList
-        common :: String
-        common = findCommonOrbit paths
-        maybeSantaDist :: Maybe Int
-        maybeSantaDist =
-            elemIndex common (Map.findWithDefault [] (Satellite "SAN") paths)
-        maybeYouDist :: Maybe Int
-        maybeYouDist =
-            elemIndex common (Map.findWithDefault [] (Satellite "YOU") paths)
-    in  fromMaybe (-1) $ do
-            santaDist <- maybeSantaDist
-            youDist <- maybeYouDist
-            pure $ case common of
-                "SAN" -> youDist
-                "YOU" -> santaDist
-                _ -> youDist + santaDist
+    fromMaybe (-1) . getJumpsBtwYouSan . getRootPaths . getTree $ orbitList
